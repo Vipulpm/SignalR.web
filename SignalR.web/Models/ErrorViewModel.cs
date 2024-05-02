b056b87a752437b8f8a3fa352e48961202aeaf0e
vipulpun <pm.vipul@gmail.com>
2024-05-02 18:11:10 +0545

    Add project files.

---
+++ SignalR.web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SignalR.web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
